f13aea828944f2354a78b0d9cb7aafb0151ada27
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-03 12:51:40 +0000

    Working on category directive

---
+++ KioskMain/directive/categoryDirective/categoryDirective.js
@@ -0,0 +1,14 @@
+angular.module('KioskMain').directive('categoryDirective', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+
+		},
+		templateUrl: 'KioskMain/directive/categoryDirective/categoryDirective.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
