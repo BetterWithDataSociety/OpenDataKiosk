ef584e0346e82f947ca39b844b8decc2aa57958a
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-14 13:23:16 +0000

    Work on layers

--- AirMap/partial/airMap/airMap.js
+++ AirMap/partial/airMap/airMap.js
@@ -6,11 +6,25 @@ angular.module('AirMap').controller('AirmapCtrl',function($scope){
   var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
   var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
 
+  // var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
+  //     streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
 
   map = new L.Map('map-canvas', {
     center: new L.LatLng(53.383611, -1.466944),
     zoom: 12
+    // ,layers: [grayscale, cities]
   });
+
+  var baseMaps = {
+    "Base": map,
+  //   "Streets": streets
+  };
+
+  var overlayMaps = {
+  //   "Cities": cities
+  };
+
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
 
   var popup = L.popup();
 
