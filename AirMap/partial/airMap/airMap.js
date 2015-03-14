af1e5ed60d4b2b269cb5407e9299eac064f62d47
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-14 10:50:01 +0000

    Got map on screen for AirMap, needs running with grunt --force serve atm tho

--- AirMap/partial/airMap/airMap.js
+++ AirMap/partial/airMap/airMap.js
@@ -21,8 +21,16 @@ angular.module('AirMap').controller('AirmapCtrl',function($scope){
           valueField: 'count'
   };
 
+  var baseLayer = L.tileLayer(
+          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+            maxZoom: 18
+          }
+        );
+
 
   heatmapLayer = new HeatmapOverlay(cfg);
+
 
   map = new L.Map('map-canvas', {
           center: new L.LatLng(53.383611, -1.466944),
