803d0e6ecd16f536c8d0e7d568492b9feed546d3
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-05-08 13:12:41 +0100

    First steps in removing leaflet

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,8 +97,11 @@ module.exports = function (grunt) {
           {src: ['img/**'], dest: 'dist/'},
           {src: ['bower_components/font-awesome/fonts/**'], dest: 'dist/',filter:'isFile',expand:true},
           {src: ['bower_components/bootstrap/fonts/**'], dest: 'dist/',filter:'isFile',expand:true},
-          {src: ['bower_components/leaflet-dist/images/**'], dest: 'dist/',filter:'isFile',expand:true},
-          {src: ['bower_components/leaflet-dist/leaflet.css'], dest: 'dist/',filter:'isFile',expand:true}
+          {src: ['bower_components/openlayers/lib/**'], dest: 'dist/',filter:'isFile',expand:true},
+          {src: ['bower_components/openlayers/img/**'], dest: 'dist/',filter:'isFile',expand:true},
+          {src: ['bower_components/openlayers/theme/**'], dest: 'dist/',filter:'isFile',expand:true},
+          // {src: ['bower_components/leaflet-dist/images/**'], dest: 'dist/',filter:'isFile',expand:true},
+          // {src: ['bower_components/leaflet-dist/leaflet.css'], dest: 'dist/',filter:'isFile',expand:true}
           // This is where we fix our leaflet problems bower_components/leaflet-dist/images/
           //{src: ['bower_components/angular-ui-utils/ui-utils-ieshiv.min.js'], dest: 'dist/'},
           //{src: ['bower_components/select2/*.png','bower_components/select2/*.gif'], dest:'dist/css/',flatten:true,expand:true},
